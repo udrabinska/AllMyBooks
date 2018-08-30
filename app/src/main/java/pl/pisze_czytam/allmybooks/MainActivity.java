a8e902e2d354412742fe8662534a5bc98ca3605e
Urszula Drabińska <u.drabinska@gmail.com>
2018-08-30 14:00:22 +0200

    Main activity with navigation

--- app/src/main/java/pl/pisze_czytam/allmybooks/MainActivity.java
+++ app/src/main/java/pl/pisze_czytam/allmybooks/MainActivity.java
@@ -1,24 +1,43 @@
 package pl.pisze_czytam.allmybooks;
 
-import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import pl.pisze_czytam.allmybooks.databinding.AllBooksBinding;
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    AllBooksBinding bind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bind = DataBindingUtil.setContentView(this, R.layout.all_books);
+        setContentView(R.layout.all_books);
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_bar);
+        setSupportActionBar(bottomAppBar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_menu, menu);
+        getMenuInflater().inflate(R.menu.bottom_nav, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_change_view:
+                Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_filter:
+                Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_settings:
+                Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
